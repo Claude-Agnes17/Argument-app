5ccfc848be1793d0fd71183d645b55f07f50d99b
Claude Agnes <rlawnstjddl12@gmail.com>
2023-01-10 09:14:39 +0900

    SWGO
    SWGO first class

---
+++ argument app/argument app/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static bool verbose = false;
+        static bool continueOnError = false;
+        static bool logging = false;
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Usage : MyApp.exe option");
+                return;
+            }
+
+            string option = args[0];
+            switch(option.ToLower())
+            {
+                case "/v":
+                case "/vsebose":
+                    verbose = true;
+                    Console.WriteLine("verbose : True!");
+                    break;
+                case "/c":
+                    continueOnError = true;
+                    Console.WriteLine("continueOnError : True!");
+                    break;
+                case "/l":
+                    logging = true;
+                    Console.WriteLine("logging : True!");
+                    break;
+                default:
+                    Console.WriteLine($"Unknown argument: {option}");
+                    break;
+            }
+
+        }
+    }
+}
